f24cb6503c40279b79b5558e7cd9055cdebb4679
sntanju <sntanju07@gmail.com>
2021-10-09 20:48:46 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeF00c25PKltNcG5kBFHmqtHhq52yZN_U",
+    authDomain: "simple-firebase-authenti-cfd96.firebaseapp.com",
+    projectId: "simple-firebase-authenti-cfd96",
+    storageBucket: "simple-firebase-authenti-cfd96.appspot.com",
+    messagingSenderId: "811280181784",
+    appId: "1:811280181784:web:331c18d303e769ee88716d"
+  };
+
+  export default firebaseConfig;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import useFirebase from '../../hooks/useFirebase';
+import './Header.css';
+
+const Header = () => {
+    const { user, logOut } = useAuth();
+    return (
+        <div className="header">
+            <Link to="/home">Home</Link>
+            <Link to="/shipping">Shipping</Link>
+            <Link to="/placeorder">Place Order</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+            <span>{user.displayName} </span>
+            { user?.email && <button onClick={logOut}>Log Out</button>}
+        </div>
+    );
+};
+
+export default Header;
